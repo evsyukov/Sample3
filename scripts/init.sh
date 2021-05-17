ab6d4579b4fe8c3d75309e610e546cd2576afc2c
Nikolay Esvyukov <evsukov@list.ru>
2021-05-17 14:41:02 +0300

    init repo

---
+++ scripts/init.sh
@@ -0,0 +1,8 @@
+curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
+# sudo apt-get update
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get install python
+sudo python get-pip.py
+# sudo python -m pip install -r ./requirements.txt
+sudo python -m pip install Flask
+sudo python -m pip install requests
